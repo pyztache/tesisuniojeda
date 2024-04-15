380bad43ad4cf962ff4b6cdab02a3f139ca27c75
pyztache <164340716+pyztache@users.noreply.github.com>
2024-04-15 03:00:51 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["uniojeda"]
+   var passwordArray = ["2024"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   window.location = "file:///C:/Users/cliente/Desktop/sistema%20jheiko%20y%20manuel/sistema/index.html"
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   count--;
+}
+}
